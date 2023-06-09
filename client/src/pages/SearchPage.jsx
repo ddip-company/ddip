ea20bb25087ada19764ac9f0b3e581fb454dfe21
TAETAEMO <pjh7179200@gmail.com>
2023-06-09 16:04:20 +0900

    feat: searchPage 구현 및 tab 메뉴 구현

---
+++ client/src/pages/SearchPage.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import Navbar from "../component/Navbar";
+import { dummyBungaeList2 } from "../static/dummy/bungaeList";
+import SearchedBungaeList from "../component/SearchedBungaeList";
+import { searchPageTabMenu as tab } from "../static/sortTab";
+import { useSearchParams } from "react-router-dom";
+
+function SearchPage() {
+  const [bungaeList, setBungaeList] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sort = searchParams.get("sort");
+
+  useEffect(() => {
+    setBungaeList(dummyBungaeList2);
+  }, []);
+
+  const makeFilledBungaeList = (bungaeList) => {
+    const filledBungaeList = bungaeList.slice();
+    while (filledBungaeList.length < 4) {
+      filledBungaeList.push(null);
+    }
+    return filledBungaeList;
+  };
+
+  const filledBungaeList = makeFilledBungaeList(bungaeList);
+
+  const switchTabHandler = (selected) => {
+    setSearchParams({ sort: selected });
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="search-container">
+        <SearchedBungaeList
+          count={bungaeList.length}
+          tab={tab}
+          onSwitch={switchTabHandler}
+          sortBy={sort}
+          bungaeList={filledBungaeList}
+        />
+      </div>
+    </>
+  );
+}
+
+export default SearchPage;
---
+++ client/src/static/sortTab.js
@@ -0,0 +1,14 @@
+export const searchPageTabMenu = [
+  {
+    name: "최신순",
+    sortBy: [null, "newest"],
+    linkTo: "newest",
+    imageSrc: "/img/line",
+    imageAlt: "line img"
+  },
+  {
+    name: "마감임박순",
+    sortBy: ["last-minute"],
+    linkTo: "last-minute"
+  }
+];
